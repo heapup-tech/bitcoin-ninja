a8e36d1fa7d68063a8461c39add006fa4bdbb365
lybenson <gwanbit@gmail.com>
2024-10-16 00:32:24 +0800

    feat: about

--- src/app/(single)/[slug]/page.tsx
+++ src/app/(single)/[slug]/page.tsx
@@ -18,7 +18,7 @@ export default async function SingleSlugPage({ params }: DocPageProps) {
   return (
     <main className={cn('relative py-6 flex-1')}>
       <div className='relative max-w-[1280px] px-4 mx-auto lg:grid lg:grid-cols-[1fr_200px] lg:gap-10'>
-        <div className='mx-auto w-full min-w-0'>
+        <div className='w-full min-w-0'>
           <div className='space-y-2'>
             <h1 className={cn('scroll-m-20 text-4xl font-bold tracking-tight')}>
               {doc.title}
@@ -31,10 +31,7 @@ export default async function SingleSlugPage({ params }: DocPageProps) {
           </div>
           <div className='mx-auto w-full min-w-0'>
             <div className='pb-12 pt-8'>
-              <Mdx
-                code={doc.body.code}
-                className='max-w-max'
-              />
+              <Mdx code={doc.body.code} />
             </div>
           </div>
         </div>
